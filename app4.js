159ea83ae882e8693b528c12c2d5a6cfb734a996
My Dude <peytonsharsh@gmail.com>
2020-07-09 13:57:34 -0500

    javascript updated in app4.js for index4.html, also a new csv was created with additional informaiton.

--- app4.js
+++ app4.js
@@ -1,5 +1,4 @@
-// Read in data first
-d3.csv("./updated_three_years.csv").then(function(tableData) {
+d3.csv("./new_restaurants_pd.csv").then(function(tableData) {
   console.log(tableData)
 
   //Create Dropdown Arrays
@@ -35,17 +34,6 @@ d3.csv("./updated_three_years.csv").then(function(tableData) {
     addresslistselect.appendChild(el);
   };
 
-  //d3.selectAll(".filter").on("change", dropdownFilter);
-  //function dropdownFilter () {
-  //    var inputBizzsubmit = d3.select("#inputBizz").node().value
-  //    addressList = []
-  //      tableData.forEach(function(inspection) {
-  //        if (inputBizzsubmit === inspection.AKA_name) {
-  //          addressList.push(inspection.Address)
-  //      }});
-
-  //  d3.select("#inputAddress").on(change)
-  //  };
 
   //Function for starting Javascript after submission
   function handleSubmit() {
@@ -55,34 +43,51 @@ d3.csv("./updated_three_years.csv").then(function(tableData) {
     console.log(inputBizzsubmit);
     };
 
-  //Populate table based on Business dropdown submission
+  //Populate table based on dropdown submissions
   function runValue() {
     var table = d3.select("#completeDataTable");
     var tbody = table.select("tbody");
     tbody.html("")
     d3.event.preventDefault();
-    var inputElement = d3.select("#inputBizz");
-    var inputValue = inputElement.property("value");
-    console.log(inputValue)
-    i = inputValue
+    var bizzElement = d3.select("#inputBizz");
+    var addressElement = d3.select("#inputAddress")
+    var bizzValue = bizzElement.property("value");
+    var addressValue = addressElement.property("value");
+    console.log(bizzValue)
+    console.log(addressValue)
+
+    bv = bizzValue
+    av = addressValue
     tableData.forEach(function(inspection) {
-      y = inspection.AKA_name
-      tableinfoentry = [inspection.Inspection_ID, inspection.AKA_name, inspection.Address, inspection.Inspection_date, inspection.Inspection_type, inspection.Results]
-      if (i === y) {
+      akaname = inspection.AKA_name
+      bizzaddress = inspection.Address
+      tableinfoentry = [inspection.Inspection_ID, inspection.AKA_name, inspection.Address, inspection.Inspection_date, inspection.Inspection_type, inspection.Results, inspection.violation_code, inspection.violation_subject, inspection.Violations]
+      if (bv === akaname && av ==="Choose...") {
+        var row = tbody.append("tr");
+        Object.entries(tableinfoentry).forEach(function([key, value]) {
+            console.log(key, value);
+            var cell = row.append("td");
+            cell.text(value)
+      })}
+      else if (bv === "Choose..." && av === bizzaddress) {
         var row = tbody.append("tr");
         Object.entries(tableinfoentry).forEach(function([key, value]) {
             console.log(key, value);
             var cell = row.append("td");
             cell.text(value);
-        })
-      }
-    })
+      })}
+      else if (bv === bizzValue && av === bizzaddress) {
+        var row = tbody.append("tr");
+        Object.entries(tableinfoentry).forEach(function([key, value]) {
+            console.log(key, value);
+            var cell = row.append("td");
+            cell.text(value);
+      })}
+    });
   };
-
 
   //Handle the Actions on the Page
   var filterbutton = d3.select("#submitbutton");
   filterbutton.on("click", handleSubmit);
 
 });
-
